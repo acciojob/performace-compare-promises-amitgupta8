7640a2098ecf294e33d574137d4a012922f5eee4
Amit Gupta <121490421+amitgupta8@users.noreply.github.com>
2023-03-13 23:37:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
-// Array of API URLs to fetch data from
 const apiUrls = [
-  "https://jsonplaceholder.typicode.com/todos/1",
-  "https://jsonplaceholder.typicode.com/todos/2",
-  "https://jsonplaceholder.typicode.com/todos/3",
-  "https://jsonplaceholder.typicode.com/todos/4",
-  "https://jsonplaceholder.typicode.com/todos/5",
-  "https://jsonplaceholder.typicode.com/todos/6",
-  "https://jsonplaceholder.typicode.com/todos/7",
-  "https://jsonplaceholder.typicode.com/todos/8",
-  "https://jsonplaceholder.typicode.com/todos/9",
-  "https://jsonplaceholder.typicode.com/todos/10",
+  'https://api.example.com/1',
+  'https://api.example.com/2',
+  'https://api.example.com/3',
+  // add more API URLs here
 ];
 
-// You can write your code here
+// function to fetch data from an API URL
+function fetchData(url) {
+  return fetch(url)
+    .then(response => response.json())
+    .catch(error => console.error(`Error fetching data from ${url}: ${error}`));
+}
+
+// function to measure time taken for Promise.all
+function measurePromiseAll() {
+  const start = Date.now();
+  Promise.all(apiUrls.map(fetchData))
+    .then(() => {
+      const end = Date.now();
+      const timeTaken = end - start;
+      document.getElementById('output-all').innerHTML = `${timeTaken}ms`;
+    });
+}
+
+// function to measure time taken for Promise.any
+function measurePromiseAny() {
+  const start = Date.now();
+  Promise.any(apiUrls.map(fetchData))
+    .then(() => {
+      const end = Date.now();
+      const timeTaken = end - start;
+      document.getElementById('output-any').innerHTML = `${timeTaken}ms`;
+    });
+}
+
+// call the functions to measure performance
+measurePromiseAll();
+measurePromiseAny();
